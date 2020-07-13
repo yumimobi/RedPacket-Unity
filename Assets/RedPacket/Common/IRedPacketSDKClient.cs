9bdbdf16ec9771422257646065cb723953bad0d7
JiaDingYi <wzy2010416033@163.com>
2020-07-13 14:36:49 +0800

    add I sdk client

---
+++ Assets/RedPacket/Common/IRedPacketSDKClient.cs
@@ -0,0 +1,27 @@
+using System;
+using RedPacket.Api;
+using UnityEngine;
+
+namespace RedPacket.Common
+{
+    public interface IRedPacketSDKClient
+    {
+        event EventHandler<EventArgs> OnSDKInitFailed;
+        event EventHandler<RPSDKEventArgs> OnSDKInitSuccess;
+        event EventHandler<EventArgs> OnLeftViewHasBeenShown;
+        event EventHandler<EventArgs> OnLeftViewHasBeenClicked;
+        event EventHandler<EventArgs> OnRedPacketControllerHasBeenShown;
+        event EventHandler<EventArgs> OnRedPacketControllerHasBeenDismissed;
+        event EventHandler<EventArgs> OnRedPacketControllerHasBeenClicked;
+        event EventHandler<EventArgs> OnFinalRedPacketControllerHasBeenShown;
+        event EventHandler<EventArgs> OnFinalRedPacketControllerHasBeenClicked;
+
+
+        bool IsReady();
+        void ShowLeftView(Transform rect);
+        void DestroyLeftView();
+        void ShowRedPacketController();
+        void ShowFinalRedPacketController();
+        
+    }
+}
